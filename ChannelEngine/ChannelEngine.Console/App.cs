5f2155762b1fce7d5857029ab21559d8827deccf
Donny Roelen <donny_roelen@hotmail.com>
2021-01-17 13:37:01 +0100

    Get Top products and update stock by console app

---
+++ ChannelEngine/ChannelEngine.Console/App.cs
@@ -0,0 +1,51 @@
+﻿using ChannelEngine.Services;
+using ChannelEngine.ViewModels.Product;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChannelEngine.Console
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+        private readonly ProductService _productService;
+
+        public App(ILogger<App> logger, ProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public async Task Run(string[] args)
+        {
+            _logger.LogInformation("Starting...");
+
+            var products = await _productService.GetTopSoldProducts(5);
+
+            System.Console.WriteLine($"Products found: {products.Count()}");
+
+            foreach (var product in products)
+            {
+                System.Console.WriteLine($"{product.Description} {product.Gtin} {product.Quantity}");
+            }
+
+            var productEditStockViewModel = products.Select(p => new ProductEditStockViewModel()
+            {
+                MerchantProductNo = p.MerchantProductNo,
+                Stock = 25
+            }).FirstOrDefault();
+
+            var result = await _productService.UpdateStock(productEditStockViewModel);
+
+            System.Console.WriteLine($"Stock for {productEditStockViewModel.MerchantProductNo} updated: {result}");
+
+            _logger.LogInformation("Finished!");
+
+            await Task.CompletedTask;
+        }
+    }
+}
--- ChannelEngine/ChannelEngine.Console/Program.cs
+++ ChannelEngine/ChannelEngine.Console/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using ChannelEngine.Services;
+using ChannelEngine.ViewModels.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ChannelEngine.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            // create service collection
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            // create service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // entry to run app
+            await serviceProvider.GetService<App>().Run(args);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // configure logging
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+
+            // build config
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.Configure<ChannelEngineOptions>(configuration.GetSection(ChannelEngineOptions.ChannelEngine));
+
+            // Services
+            services.AddSingleton<ChannelEngineService, ChannelEngineService>();
+            services.AddScoped<ProductService, ProductService>();
+            // services.AddTransient<IMyRespository, MyConcreteRepository>();
+
+            // add app
+            services.AddTransient<App>();
         }
     }
 }
